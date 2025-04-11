66248647076b047fce03830118549779d36db52f
naruto-uzamaki <kalavalarevanth@gmail.com>
2025-04-11 18:39:39 +0530

    Added Search and Flags Impl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import CountryCard from './CountryCard';
 
 function App() {
+  const countries = useRef([]);
+  const [searchData, setSearchData] = useState([]);
+  const [query, setQuery] = useState("");
+  const apiEndPoint = "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries";
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const res = await fetch(apiEndPoint);
+        const data = await res.json();
+        countries.current = data;
+        setSearchData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    init();
+  }, []);
+
+  const searchCountries = () => {
+    console.log(countries.current, query);
+    const filteredData = countries.current.filter((country) => {
+      return country.common.toLowerCase().includes(query.toLowerCase());
+    });
+    setSearchData(filteredData);
+
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => searchCountries(), 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [query]);
+
+
+  const renderedFlags = searchData.map(country => {
+    return <CountryCard key={country.common} name={country.common} img={country.png} />
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <form className="searchContainer" onSubmit={(e) => {
+        e.preventDefault();
+        searchCountries();
+      }}>
+
+        <input className="search" type='text' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search for countries'></input>
+      </form>
+
+      <div className='container'>
+        {renderedFlags}
+      </div>
     </div>
   );
+
 }
 
 export default App;
